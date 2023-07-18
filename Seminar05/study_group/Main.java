456e05ef99950e4ae245df2d7d1896d23efa1238
Cristin <anita_kineva@mail.ru>
2023-07-18 11:56:09 +0300

    added Seminar 5

---
+++ Seminar05/study_group/Main.java
@@ -0,0 +1,12 @@
+package study_group;
+
+
+import study_group.model.service.Service;
+import study_group.view.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.start();
+    }
+}
